90f4464f2fe18bb05eb097bfd82a61b879cae261
Peter O <that0n3guy@users.noreply.github.com>
2015-09-03 16:00:10 -0500

    run composer

--- .docker/after-boot-actions/init.sh
+++ .docker/after-boot-actions/init.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+# these are actions that need to be done after boot, not after build.  This is because we don't have DB access in build
+#    because env vars aren't available yet.
+APPDIR="/app"
+
+#run composer
+cd /app COMPOSER_HOME="/root" composer install --no-dev --prefer-dist --optimize-autoloader --no-interaction
+
  # from docker-entrypoint on mautic/docker file
 
 # Write the database connection to the config so the installer prefills it
@@ -8,4 +15,6 @@ if ! [ -e /app/app/config/local.php ]; then
         # Make sure our web user owns the config file if it exists
         chown www-data:www-data /app/app/config/local.php
 fi
+
+chown -R www-data:www-data /app
 
